c00aa8d9db231e533f36251b0254e460b6866f19
Iliya Akkuzin <gr3yknigh1@gmail.com>
2021-08-01 08:17:26 +0300

    Init commit. Add code

---
+++ cvrt2mp4/cvrt2mp4.py
@@ -0,0 +1,61 @@
+"""
+Original
+https://gist.github.com/wahengchang/19a65f561fdb57846e0f7c512028cb0d
+"""
+import subprocess
+import argparse
+import shutil
+import os
+
+
+is_exe_exists = lambda name: shutil.which(name) is not None
+
+
+if not is_exe_exists("ffmpeg"):
+    raise Exception("This program require 'ffmpeg' executable in PATH variable")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("target", help="target directory")
+parser.add_argument("--output", help="output directory", default="./mp4")
+args = parser.parse_args()
+
+
+src = args.target
+dst = args.output 
+
+
+if not os.path.exists(dst):
+    os.mkdir(dst)
+
+
+# TODO(Gr3yKnigh1): Create progress bar and verbose argument
+def main():
+    for root, dirs, filenames in os.walk(src, topdown=False):
+        for filename in filenames:
+            print('[INFO] 1', filename)
+            
+            try:
+                frmt = '' # format
+                if ".flv" in filename.lower():
+                    frmt=".flv"
+                elif ".mp4" in filename.lower():
+                    frmt=".mp4"
+                elif ".avi" in filename.lower():
+                    frmt=".avi"
+                elif ".mov" in filename.lower():
+                    frmt=".mov"
+                else:
+                    continue
+
+                inputfile = os.path.join(root, filename)
+                print('[INFO] 1', inputfile)
+                outputfile = os.path.join(dst, filename.lower().replace(frmt, ".mp4"))
+                subprocess.call(['ffmpeg', '-i', inputfile, outputfile])  
+            except Exception as e:
+                print("An exception occurred")
+                print(e)
+
+
+if __name__ == '__main__':
+    main()
